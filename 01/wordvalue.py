44ee6b8c9542ca86419d86c6b3b31ddf820b15f9
thechosenone <zacfire1998@hotmail.fr>
2019-07-15 20:24:39 -0400

    Fixed some misunderstandings and now I pass every test :)

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,15 +1,16 @@
 from data import DICTIONARY, LETTER_SCORES
 
-def load_words(path):
+def load_words(path=DICTIONARY):
     """Load dictionary into a list and return list"""
     word_list = []
     with open(path) as dic:
-        word_list = dic.read().upper().split()
+        word_list = dic.read().split()
     return word_list
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
+    word = word.upper()
     score = 0
     for letter in word:
         try:
@@ -18,18 +19,21 @@ def calc_word_value(word):
             pass
     return score
 
-def max_word_value(words):
+def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
+    if words == None:
+        words = load_words()
     previous_best_score = 0
     previous_best_word = ""
     for word in words:
         score = calc_word_value(word)
         if score > previous_best_score:
             previous_best_word = word
-    return (word, score)
+            previous_best_score = score
+    return previous_best_word
 
 if __name__ == "__main__":
     words = load_words(DICTIONARY)
-    word, highest_score = max_word_value(words)
-    print(f"The word with the highest score is {word} with an outstanding {highest_score} points!")
+    word = max_word_value(words)
+    print(f"The word with the highest possible score in english is {word}!")
